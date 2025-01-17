299699d056342c5c47214bb99134a8f762d96757
Viswa-1203 <viswaganisetti1203@gmail.com>
2025-01-17 14:45:14 +0530

    new

--- app/models/resume.py
+++ app/models/resume.py
@@ -1,31 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from typing import List, Optional
 
-class PersonalInformation(BaseModel):
+class Resume(BaseModel):
     full_name: str
     email: str
     phone_number: int
     website: Optional[str] = None
-
-class Education(BaseModel):
     degree: str
     college: str
     graduation_year: int
-
-class WorkExperience(BaseModel):
     job_title: str
     company: str
     start_date: str
     end_date: str
     description: str
-
-class Resume(BaseModel):
-    personal_information: PersonalInformation
-    education: List[Education]
-    work_experience: List[WorkExperience]
-    skills: List[str]
-    projects: List[str]
-    certifications: List[str]
-    languages: List[str]
-    hobbies: List[str]
-    references: List[str]
+    education: List[str]  
+    work_experience: List[str]  
+    skills: str
+    projects: str
+    certifications: str
+    languages: str
+    hobbies: str
+    references: str
